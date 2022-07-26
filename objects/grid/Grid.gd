5354e8fc6509ab658576ae435791686e11717e0b
Ismail Filipov <64804972+ismailgamedev@users.noreply.github.com>
2022-07-26 22:08:53 +0300

    UI Degistirecegiz

--- objects/grid/Grid.gd
+++ objects/grid/Grid.gd
@@ -2,6 +2,8 @@ extends Area2D
 
 onready var building_sprite = get_node("BuildingSprite")
 
+enum current_building_enum { bos,kucuk_ev,orta_ev,buyuk_ev,fabrika,petrol_donusturucusu,biyo_yakit_donusturucusu, }
+export(current_building_enum) var dropoff = current_building_enum.bos
 
 func _on_Grid_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
