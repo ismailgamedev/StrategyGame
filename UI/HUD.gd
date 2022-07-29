f05e4a4ab581e36bd91aa12535749c0c5afd8f69
Ismail Filipov <64804972+ismailgamedev@users.noreply.github.com>
2022-07-29 03:05:27 +0300

    ui ve bir suru sey

--- UI/HUD.gd
+++ UI/HUD.gd
@@ -2,6 +2,12 @@ extends Control
 
 var build_btn_bool = false
 var evmenu_btn_bool = false
+onready var money_label = get_node("Money/MoneyLabel")
+onready var nufus_label = get_node("Nufus/NufusLabel")
+onready var elektirik_label = get_node("Elektirik/ElektirikLabel")
+onready var yemek_label = get_node("Yemek/YemekLabel")
+onready var su_label = get_node("Su/SuLabel")
+onready var mutluluk_label = get_node("Mutluluk/MutlulukLabel")
 onready var anim_player = get_node("AnimationPlayer")
 onready var ev_menu_btn = get_node("Control/EvMenuBtn")
 
@@ -12,20 +18,33 @@ func _on_BuildBtn_pressed():
 		build_btn_bool = true
 	else:
 		anim_player.play_backwards("evmenubtn")
+		Globals.current_building = Globals.current_building_enum.bos
 		yield(anim_player,"animation_finished")
 		anim_player.play_backwards("buildbtnpressanim")
 		build_btn_bool = false
 	
-
-
+func _process(delta):
+	money_label.text = str(Globals.data["dolar"])
+	nufus_label.text = str(Globals.data["nufus"])
+	elektirik_label.text = str(Globals.data["enerji"])
+	yemek_label.text = str(Globals.data["yemek"])
+	su_label.text = str(Globals.data["su"])
+	mutluluk_label.text = str(Globals.data["mutluluk"])
 func _on_EvMenuBtn_pressed():
 	if evmenu_btn_bool == false:
 		anim_player.play("evmenubtn")
 		evmenu_btn_bool = true
 	else:
 		anim_player.play_backwards("evmenubtn")
+		Globals.current_building = Globals.current_building_enum.bos
 		evmenu_btn_bool = false
 
 
 func _on_KucukEvBtn_pressed():
-	Globals.current_building = Globals.current_building_enum.kucuk_ev
+	if Globals.data["dolar"] >= 20:
+		Globals.current_building = Globals.current_building_enum.kucuk_ev
+
+
+func _on_OrtaEvBtn_pressed():
+	if Globals.data["dolar"] >= 40:
+		Globals.current_building = Globals.current_building_enum.orta_ev
--- objects/grid/Grid.gd
+++ objects/grid/Grid.gd
@@ -1,40 +1,78 @@
 extends Area2D
 
 onready var building_sprite = get_node("BuildingSprite")
-
+onready var building_timer = get_node("binaTimer")
+onready var nowbuildingicon = get_node("BuildingShits")
+onready var platform = get_node("Platform")
 #Textures
 onready var kucuk_ev_texture = preload("res://assets/sprites/buildings/ev2.png")
-onready var orta_ev_texture = preload("res://assets/sprites/buildings/ev3.png")
+onready var orta_ev_texture = preload("res://assets/sprites/buildings/ev4.png")
 onready var buyuk_ev_texture = preload("res://assets/sprites/buildings/apartman.png")
 #Grid Building Type
+
 enum current_building_enum { bos,kucuk_ev,orta_ev,buyuk_ev,fabrika,petrol_donusturucusu,biyo_yakit_donusturucusu, }
 export(current_building_enum) var current_building = current_building_enum.bos
 
+var is_building : bool
 
+func _process(delta):
+	if is_building == false:
+		nowbuildingicon.visible = false
+		building_sprite.modulate = Color(1, 1, 1)
+	else:
+		nowbuildingicon.visible = true
+		nowbuildingicon.get_node("timer_label").text = str(int(building_timer.time_left))
+		building_sprite.modulate = Color(1, 1, 1, 0.3)
+	if Globals.current_building != Globals.current_building_enum.bos:
+		platform.modulate = Color(0.203922, 0.490196, 0.203922)
+	else:
+		platform.modulate = Color(1, 1, 1)
+		
 
 func _on_Grid_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton:
 		if event.pressed:
 			# Eger insaa ettigimiz bina yoksa girmiyoruz
 			if Globals.current_building != Globals.current_building_enum.bos and current_building == current_building_enum.bos:
-				
-				if Globals.current_building == Globals.current_building_enum.kucuk_ev:
-					building_sprite.texture = kucuk_ev_texture
-					current_building = current_building_enum.kucuk_ev
-					Globals.current_building = Globals.current_building_enum.bos
+				create_building()
 
-				if Globals.current_building == Globals.current_building_enum.orta_ev:
-					building_sprite.texture = orta_ev_texture
-					current_building = current_building_enum.orta_ev
-					Globals.current_building = Globals.current_building_enum.bos
+func create_building():
+	if Globals.current_building == Globals.current_building_enum.kucuk_ev and Globals.data["dolar"] >= 20:
+		building_sprite.texture = kucuk_ev_texture
+		current_building = current_building_enum.kucuk_ev
+		Globals.current_building = Globals.current_building_enum.bos
+		is_building = true
+		building_timer.start(8)
+		Globals.data["dolar"] -= 20
 
-				if Globals.current_building == Globals.current_building_enum.buyuk_ev:
-					building_sprite.texture = buyuk_ev_texture
-					current_building = current_building_enum.buyuk_ev
-					Globals.current_building = Globals.current_building_enum.bos
+	if Globals.current_building == Globals.current_building_enum.orta_ev and Globals.data["dolar"] >= 40:
+		building_sprite.texture = orta_ev_texture
+		current_building = current_building_enum.orta_ev
+		Globals.current_building = Globals.current_building_enum.bos
+		building_timer.start(8)
+		is_building = true
+		Globals.data["dolar"] -= 40
 
-		
+	if Globals.current_building == Globals.current_building_enum.buyuk_ev and Globals.data["dolar"] >= 100:
+		building_sprite.texture = buyuk_ev_texture
+		current_building = current_building_enum.buyuk_ev
+		Globals.current_building = Globals.current_building_enum.bos
+		is_building = true
+		building_timer.start(8)
+		Globals.data["dolar"] -= 100
 
 func _on_binaTimer_timeout():
 	if current_building == current_building_enum.kucuk_ev:
-		pass
+		is_building = false
+		Globals.data["nufus"] += 4
+		building_timer.stop()
+	if current_building == current_building_enum.orta_ev:
+		is_building = false
+		Globals.data["nufus"] += 6
+		building_timer.stop()
+	if current_building == current_building_enum.buyuk_ev:
+		is_building = false
+		Globals.data["nufus"] += 15
+		building_timer.stop()
+		
+
