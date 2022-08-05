a675310a90836f2d4720e7b671ffc9ab3d6b74d9
beetlejuicetr <keremdemir7526@gmail.com>
2022-08-05 20:18:15 +0300

    sx

--- UI/MainMenu.gd
+++ UI/MainMenu.gd
@@ -12,13 +12,15 @@ func _ready():
 	DataHandler._load()
 	TimeHandler.httpRequest.request("http://worldtimeapi.org/api/timezone/Europe/Istanbul")
 	print(TimeHandler.timeFinaly)
+	Debug.add_command(self,"saypop",[])
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
+func saypop():
+	Debug.put_log("pop")
 
 func _on_Button_pressed():
 	get_tree().change_scene("res://Scenes/Game/World.tscn")
