1aa816dec480a957c947cc4fff066efba26a903c
beetlejuicetr <keremdemir7526@gmail.com>
2022-08-06 20:29:47 +0300

    firebase plugini tekrardan yuklendi

--- UI/MainMenu.gd
+++ UI/MainMenu.gd
@@ -4,10 +4,19 @@ extends Control
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var firebase = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
+	
+	if Engine.has_singleton("Firebase"):
+		firebase = Engine.get_singleton("Firebase")
+		print("Loaded Firebase")
+		if firebase:
+			firebase.init(get_instance_id())
+		firebase.cloudmessaging_subscribe_to_topic("topicName")
+	
 	DataHandler._load_time()
 	DataHandler._load()
 	TimeHandler.httpRequest.request("http://worldtimeapi.org/api/timezone/Europe/Istanbul")
